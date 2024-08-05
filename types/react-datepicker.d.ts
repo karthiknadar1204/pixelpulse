05d10eedc34c56452e71b8253ef94382ac589c33
karthik <karthiknadar1204@gmail.com>
2024-08-05 15:37:00 +0530

    react-datepicker.d.ts file fixed

--- types/react-datepicker.d.ts
+++ types/react-datepicker.d.ts
@@ -1,1 +1,22 @@
-declare module 'react-datepicker';
+declare module 'react-datepicker' {
+    import React from 'react';
+  
+    // Type definitions for ReactDatePickerProps
+    export interface ReactDatePickerProps {
+      selected?: Date;
+      onChange?: (date: Date | null) => void;
+      showTimeSelect?: boolean;
+      timeFormat?: string;
+      timeIntervals?: number;
+      timeCaption?: string;
+      dateFormat?: string;
+      className?: string;
+      // Add any other props you use from react-datepicker
+    }
+  
+    // Define the ReactDatePicker component
+    const ReactDatePicker: React.FC<ReactDatePickerProps>;
+  
+    export default ReactDatePicker;
+  }
+  
